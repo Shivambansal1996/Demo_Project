38d665dba80c8419a5e35254ba70fa78ded1f360
Shivam Bansal <shivam.bansal2496@gmail.com>
2020-01-25 17:48:52 +0530

    All files added

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import '../Login/Login.css'
 import Augmate from './Augmate.png'
 import axios from 'axios'
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form'
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import Eye from './eye.png'
+import Back from './Back.jpg'
 
 export class Login extends Component {
 
@@ -13,17 +15,80 @@ export class Login extends Component {
         this.state = {
 
             email_id:'',
-            password:''
-             
+            password:'',
+            emailError:'',
+            passwordError:'',
+            isPasswordShown:false,
+            isPasswordStrength:''             
         }
+        
     }
 
-    clickHandler=(event)=>{
+
+    handleChange=(e)=>{
 
         this.setState({
-            [event.target.name]:event.target.value
+            [e.target.name]:e.target.value
         })
+
+        if(e.target.name==='email_id'){
+            var emailRegex=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            if(e.target.value==='' || e.target.value===null ){
+              this.setState({
+                emailError:'Please enter an Email id'
+              })
+            }
+            else if(!emailRegex.test(e.target.value)){
+              this.setState({
+                emailError:"Please enter a valid Email id"
+              })
+        
+            } 
+            else{
+              this.setState({
+                emailError:null
+              })
+            }
+          }
+
+          if(e.target.name==='password'){
+            var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+            var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+        
+        
+            if(e.target.value==='' || e.target.value===null ){
+              this.setState({
+                passwordError:'Please enter Password'
+              })
+            }
+            else if(strongRegex.test(e.target.value)){
+              this.setState({
+                passwordError:'Strong Password'
+              })
+        
+            }
+        
+            else if(mediumRegex.test(e.target.value)){
+              this.setState({
+                passwordError:'Weak Password'
+              })
+        
+            }
+            else{
+              this.setState({
+                passwordError:'Very Weak Password'
+              })
+            }
+          }
     }
+
+    eye=()=>{
+       
+        this.setState({
+  
+          isPasswordShown:!this.state.isPasswordShown
+        })
+        }
 
 
     login=(event)=>{
@@ -45,22 +110,27 @@ export class Login extends Component {
     
     render() {
         return (
+            <div>
+        <img src={Back} width="100%" height="100%"></img>
             <div className='container' style={{position:"relative"}}>
             
             <div className="custom1">
-            <h1 className="font-weight-500">Login to your Account</h1>
-            <h6>Don't have an Account ? <code className="text-primary font-weight-bold" style={{cursor:"pointer"}} onClick={this.signUp}>Sign Up Free</code></h6>
+            <h1>Login to your Account</h1>
+            <h6>Don't have an Account ? <code style={{cursor:"pointer", color:'powderblue', fontWeight:'bolder', textDecoration:'underline'}} onClick={this.signUp}>Sign Up Free</code></h6>
             </div>
 
             <Form className="custom2">
              <FormGroup>
-               <Label  for="exampleEmail" className="float-left">Email id</Label>
-               <Input type="email"  name="email_id" placeholder="Email id" value={this.state.email_id} onChange={this.clickHandler} required/>
+               <Label  for="exampleEmail" className="float-left">Email id<code>*</code></Label>
+               <Input type="email"  name="email_id" placeholder="Email id" value={this.state.email_id} onChange={(e)=>{this.handleChange(e)}} required/>
+               <div style={{color:'red', textAlign:'left'}}>{this.state.emailError}</div>
              </FormGroup>
 
             <FormGroup>
-             <Label for="examplePassword" className="float-left">Password</Label>
-             <Input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.clickHandler} required/>
+             <Label for="examplePassword" className="float-left">Password<code>*</code></Label>
+             <Input type={(this.state.isPasswordShown)?"text" : "password"} name="password"  placeholder="Password" value={this.state.password} onChange={(e)=>{this.handleChange(e)}} required/>
+             <img src={Eye} style={{ width: "20px", marginTop: "-67px", marginLeft: "433px"}} onClick={this.eye}></img>
+             <div className='custome4'>{this.state.passwordError}</div>
              </FormGroup>
 
             <FormGroup check className="float-left">
@@ -74,8 +144,11 @@ export class Login extends Component {
             <Button color="primary" size="lg" style={{marginTop:"55px"}} disabled={!(this.state.email_id && this.state.password)} onClick={this.login} >login with Email</Button>{' '}
             </Form>
             </div>
+            </div>
         )
     }
 }
 
-export default Login
+export default reduxForm({
+    form: 'login'  // a unique identifier for this form
+  })(Login)
--- src/Component/Signup/Signup.js
+++ src/Component/Signup/Signup.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import './Signup.css'
 import axios from 'axios'
+import { Field, reduxForm } from 'redux-form'
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import Eye from './eye.png'
+import Back from './Back.jpg'
 
 export class Signup extends Component {
 
@@ -14,18 +17,162 @@ export class Signup extends Component {
              email_id:'',
              mobile_no:'',
              password:'',
-             confirm_password:''
-             
+             confirm_password:'',
+             firstnameError:'',
+             lastnameError:'',
+             emailError:'',
+             mobileError:'',
+             passwordError:'',
+             confirmPasswordError:'',
+             isPasswordShown:false,
+             isConfirmPasswordShown:false
+
         }
     }
+    
 
-    clickHandler=(event)=>{
+    handleChange=(e)=>{
 
-        this.setState({
-           [event.target.name] : event.target.value
-        })
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+  
+    if(e.target.name==='first_name'){
+    if(e.target.value==='' || e.target.value===null  ){
+      this.setState({
+        firstnameError:'Please enter first name'
+      })
+    }
+    else{
+      this.setState({
+        firstnameError:null
+      })
+    }
+  }
+
+  if(e.target.name==='last_name'){
+    if(e.target.value==='' || e.target.value===null ){
+      this.setState({
+        lastnameError:'Please enter last name'
+      })
+    }
+    else{
+      this.setState({
+        lastnameError:null
+      })
+    }
+  }
+
+  if(e.target.name==='email_id'){
+    var emailRegex=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(e.target.value==='' || e.target.value===null ){
+      this.setState({
+        emailError:'Please enter an Email id'
+      })
+    }
+    else if(!emailRegex.test(e.target.value)){
+      this.setState({
+        emailError:"Please enter a valid Email id"
+      })
+
+    } 
+    else{
+      this.setState({
+        emailError:null
+      })
+    }
+  }
+
+  if(e.target.name==='mobile_no'){
+    if(e.target.value==='' || e.target.value===null ){
+      this.setState({
+        mobileError:'Please enter 10 digit mobile no'
+      })
+    }
+    else{
+      this.setState({
+        mobileError:null
+      })
+    }
+  }
+
+  if(e.target.name==='password'){
+    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+
+
+    if(e.target.value==='' || e.target.value===null ){
+      this.setState({
+        passwordError:'Please enter Password'
+      })
+    }
+    else if(strongRegex.test(e.target.value)){
+      this.setState({
+        passwordError:'Strong Password'
+      })
 
     }
+
+    else if(mediumRegex.test(e.target.value)){
+      this.setState({
+        passwordError:'Weak Password'
+      })
+
+    }
+    else{
+      this.setState({
+        passwordError:'Very Weak Password'
+      })
+    }
+  }
+
+  if(e.target.name==='confirm_password'){
+    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+    var mediumRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+
+
+    if(e.target.value==='' || e.target.value===null ){
+      this.setState({
+        confirmPasswordError:'Please enter Password'
+      })
+    }
+    else if(strongRegex.test(e.target.value)){
+      this.setState({
+        confirmPasswordError:'Strong Password'
+      })
+
+    }
+
+    else if(mediumRegex.test(e.target.value)){
+      this.setState({
+        confirmPasswordError:'Weak Password'
+      })
+
+    }
+    else{
+      this.setState({
+        confirmPasswordError:'Very Weak Password'
+      })
+    }
+  }
+    }
+
+    eye=()=>{
+       
+      this.setState({
+
+        isPasswordShown:!this.state.isPasswordShown
+      })
+      }
+
+      eye1=()=>{
+       
+        this.setState({
+  
+          isConfirmPasswordShown:!this.state.isConfirmPasswordShown
+        })
+        }
+    
 
     back=()=>{
 
@@ -51,56 +198,63 @@ export class Signup extends Component {
     }
     }
 
-   
 
    
-    
     render() {
         return (
             <div className="custome1">
-        
+        <img src={Back} width="100%" height="100%"></img>
+
+        <div className='custome5'>
+        <h1>Create an Account</h1> 
+        <h6>Already have an Account? <code style={{cursor:"pointer", color:'powderblue', textDecoration:'underline', fontWeight:'bolder'}} onClick={this.back}>Sign In!</code></h6>
+        </div>
+
          <div className='container'>
 
-         <h1 className="custome2">Create an Account</h1>
-       
-
        <Form className="custome3">
-         <FormGroup className="custome4">
-          <Label for="exampleFirstName" className="float-left">First Name</Label>
-          <Input type="text" name="first_name" placeholder="First Name" value={this.state.first_name} onChange={this.clickHandler} required/>
+       
+         <FormGroup>
+          <Label for="exampleFirstName" className="float-left">First Name<code>*</code></Label>
+          <Input type="text" name="first_name" placeholder="First Name" value={this.state.first_name} onChange={(e)=>{this.handleChange(e)}} required/>
+          <div style={{color:'red'}}>{this.state.firstnameError}</div>
         </FormGroup>
 
         <FormGroup>
-        <Label for="exampleLastName" className="float-left">Last Name</Label>
-        <Input type="text" name="last_name" placeholder="Last Name" value={this.state.last_name} onChange={this.clickHandler} required/>
+        <Label for="exampleLastName" className="float-left">Last Name<code>*</code></Label>
+        <Input type="text" name="last_name" placeholder="Last Name" value={this.state.last_name} onChange={(e)=>{this.handleChange(e)}} required/>
+        <div style={{color:'red'}}>{this.state.lastnameError}</div>
       </FormGroup>
 
       <FormGroup>
-        <Label for="Enter Email id" className="float-left">Email</Label>
-        <Input type="email" name="email_id" placeholder="Email Id" value={this.state.email_id} onChange={this.clickHandler} required/>
+        <Label for="Enter Email id" className="float-left">Email<code>*</code></Label>
+        <Input type="email" name="email_id" placeholder="Email Id" value={this.state.email_id} onChange={(e)=>{this.handleChange(e)}} required/>
+        <div style={{color:'red'}}>{this.state.emailError}</div>
       </FormGroup>
 
       <FormGroup>
-        <Label for="Enter Mobile no" className="float-left">Mobile No</Label>
-        <Input type="text" name="mobile_no" placeholder="Mobile No" value={this.state.mobile_no} onChange={this.clickHandler} required/>
+        <Label for="Enter Mobile no" className="float-left">Mobile No<code>*</code></Label>
+        <Input type="text" name="mobile_no" placeholder="Mobile No" value={this.state.mobile_no} onChange={(e)=>{this.handleChange(e)}} required/>
+        <div style={{color:'red'}}>{this.state.mobileError}</div>
       </FormGroup>
 
       <FormGroup>
-        <Label for="examplePassword" className="float-left">Password</Label>
-        <Input type="password" name="password"  placeholder="Password" value={this.state.password} onChange={this.clickHandler} required/>
+        <Label for="examplePassword" className="float-left">Password<code>*</code></Label>
+        <Input type={(this.state.isPasswordShown)?"text" : "password"} name="password"  placeholder="Password" value={this.state.password} onChange={(e)=>{this.handleChange(e)}} required/>
+       <img src={Eye} style={{ width: "20px", marginTop: "-67px", marginLeft: "433px"}} onClick={this.eye}></img>
+        <div className='custome4'>{this.state.passwordError}</div>
       </FormGroup>
 
       <FormGroup>
-        <Label for="examplePassword" className="float-left">Confirm Password</Label>
-        <Input type="password" name="confirm_password"  placeholder="Confirm Password" value={this.state.confirm_password} onChange={this.clickHandler} required/>
+        <Label for="examplePassword" className="float-left">Confirm Password<code>*</code></Label>
+        <Input type={(this.state.isConfirmPasswordShown)?"text" : "password"} name="confirm_password"  placeholder="Confirm Password" value={this.state.confirm_password} onChange={(e)=>{this.handleChange(e)}} required/>
+        <img src={Eye} style={{ width: "20px", marginTop: "-67px", marginLeft: "433px"}} onClick={this.eye1}></img>
+        <div className='custome4'>{this.state.confirmPasswordError}</div>
       </FormGroup>
 
       <h6 className="font-weight-normal text-left">By creating an account, you agree to our <code className="text-primary font-weight-bold">Terms & Conditions</code></h6>
-      <Button style={{marginTop:"5px"}} color="primary" size="lg" disabled={!(this.state.first_name && this.state.last_name && this.state.email_id && this.state.mobile_no && this.state.password && this.state.confirm_password)} onClick={this.signUp} >Create an Account</Button>{' '}
-      <hr></hr>
-                <h6 className="font-weight-normal">Already have an Account? <code style={{cursor:"pointer"}} onClick={this.back} className="text-primary font-weight-bold">Sign In!</code></h6>
-        
-      
+      <Button style={{marginTop:"5px"}} style={{ backgroundImage: "linear-gradient(to right,#9eafe5,#95bbe2,#82d7dc,#74e9d6)"}} size="lg" disabled={!(this.state.first_name && this.state.last_name && this.state.email_id && this.state.mobile_no && this.state.password && this.state.confirm_password)} onClick={this.signUp} >Create an Account</Button>{' '}
+              
       </Form>   
                  </div>
          </div>
@@ -108,4 +262,6 @@ export class Signup extends Component {
     }
 }
 
-export default Signup
+export default reduxForm({
+  form: 'signup'  // a unique identifier for this form
+})(Signup)
